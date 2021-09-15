eeb7df9f5964c6bbf7f34e1ec576ee0d438cc7d7
buttercubz <ericksosagarcias@gmail.com>
2021-09-14 21:08:29 -0400

    feat: update deno-rollup

--- deno-rollup/examples/html/rollup.config.ts
+++ deno-rollup/examples/html/rollup.config.ts
@@ -7,7 +7,27 @@ const format = "es" as const;
 export default {
   input: new URL("./src/mod.ts", `${__dirname}/`).href,
   plugins: [
-    html(),
+    html({
+      template: ({}) => {
+        return `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+            <title>Svelte app custom</title>
+
+            <link rel="icon" type="image/png" href="/favicon.png" />
+            <link rel="stylesheet" href="/global.css" />
+            <link rel="stylesheet" href="/build/bundle.css" />
+
+            <script defer src="/build/bundle.js"></script>
+          </head>
+
+          <body></body>
+        </html>`;
+      },
+    }),
   ],
   output: {
     dir: "./dist",
--- deno-rollup/plugins/svelte/deps.ts
+++ deno-rollup/plugins/svelte/deps.ts
@@ -1,11 +1,11 @@
-export * as path from "https://deno.land/std@0.91.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
 
 export {
   compile,
   preprocess,
-} from "https://deno.land/x/snel@v0.5.0/compiler/compiler.ts";
+} from "https://deno.land/x/snel@v0.5.7/compiler/compiler.ts";
 
 export type {
   compileOptions,
   PreprocessorGroup,
-} from "https://deno.land/x/snel@v0.5.0/compiler/types.ts";
+} from "https://deno.land/x/snel@v0.5.7/compiler/types.ts";
